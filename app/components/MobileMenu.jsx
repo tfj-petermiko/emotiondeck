abd06dea39451b4d408744b8f014f65728c49607
tfj-petermiko <tfj.petermiko@proton.me>
2025-10-18 14:44:11 +0100

    Auto deploy - 18-10-2025_1444

--- app/components/MobileMenu.jsx
+++ app/components/MobileMenu.jsx
@@ -18,6 +18,7 @@ export default function MobileMenu() {
   const tabs = [
     { icon: "🏠", name: "Home", desc: "Explore EmotionDeck’s World of Human Emotion", href: "/" },
     { icon: "💠", name: "Free Collection", desc: "Begin Your Journey Into Emotion Perception", href: "/free" },
+    { icon: "🎓", name: "Learn & Grow", desc: "Boost Emotional Intelligence through Practice", href: "/learn" },
     { icon: "🌍", name: "Global Map", desc: "One Country, One Emotion", href: "/globalmap" },
     { icon: "🔓", name: "Get PRO Access", desc: "Unlock Full EmotionDeck Experience", href: "/pro" },
   ];
