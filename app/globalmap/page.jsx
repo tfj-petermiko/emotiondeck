2cb0c774c8de51cf1167dd493dd181d6dc4cc908
tfj-petermiko <tfj.petermiko@proton.me>
2025-10-19 07:51:26 +0100

    Auto deploy - 19-10-2025_751

--- app/globalmap/page.jsx
+++ app/globalmap/page.jsx
@@ -1,10 +1,12 @@
+"use client";
 import WorldMap from "../components/WorldMap/WorldMap";
 
 export default function WorldPage() {
   return (
-    <main className="min-h-screen text-white">
-      <WorldMap />
+    <main className="flex-grow flex flex-col text-white overflow-hidden">
+      <div className="flex-1 w-full overflow-hidden">
+        <WorldMap />
+      </div>
     </main>
   );
 }
-
